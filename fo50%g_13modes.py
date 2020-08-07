a79daf6a61fe634d55df23ca09f39bdd7755269a
yangtriple <41220197+yangtriple@users.noreply.github.com>
2020-08-07 11:08:50 +0800

    final model
    add noise to the input signal

--- fo50%g_13modes.py
+++ fo50%g_13modes.py
@@ -16,26 +16,32 @@ import matplotlib.pyplot as plt
 
 
 '''
-fouropamp 滤波器实验
+fouropamp circuit experiment
 
 '''
 folder_path=r'E:\analogdata\fouropamp1500\all'
 filename=r'fo50.csv'
 df_50=load_data(folder_path,filename)
-print('数据加载大小:',df_50.shape)
+print('load data size:',df_50.shape)
 
 x_train,x_test,y_train,y_test=dataselected(df_50,n_cut=0,n_down=7,n_s=200,n_f=200,n_c=13)
 
-print('训练集数据大小：',x_train.shape)
-print('测试集数据大小:',x_test.shape)
+print('training set size：',x_train.shape)
+print('testing set size:',x_test.shape)
 y_train_sc=to_categorical(y_train,num_classes=13)#把类别标签转换为onehot编码，onehot编码是一种计算机处理的二元编码
 y_test_sc=to_categorical(y_test,num_classes=13)
 
+'''
+add noise to input signals
+'''
+noise_factor = 0.5
+x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
+x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
 
 ''''
-自编码器模型
+denoising autoencoder training and testing
 '''
-result=TBAE(x_train,x_test,y_train_sc,y_test_sc,class_n=13,hidden_layer=[100,50],epoc=200,batch_s=100, 
+result=TBAE(x_train_noisy,x_test,y_train_sc,y_test_sc,class_n=13,hidden_layer=[100,50],epoc=200,batch_s=100, 
          lr_a=0.005,decay_a=1e-6)
 score=result[3]
-print('准确率',score)
+print('accuracy',score)
--- sk50%_11modes_724.py
+++ sk50%_11modes_724.py
@@ -17,21 +17,29 @@ sallen-key 滤波器实验
 folder_path=r'E:\analogdata\AC_t5%\sallen-key\all600'
 filename=r'sk50%_11modes723.csv'
 df_50=load_data(folder_path,filename)
-print('数据加载大小:',df_50.shape)
+print('load data size:',df_50.shape)
 
 x_train,x_test,y_train,y_test=dataselected(df_50,n_cut=17,n_down=7,n_s=150,n_f=85,n_c=11)
 
-print('训练集数据大小：',x_train.shape)
-print('测试集数据大小:',x_test.shape)
+print('training set size：',x_train.shape)
+print('testing set size:',x_test.shape)
 y_train_sc=to_categorical(y_train,num_classes=11)#把类别标签转换为onehot编码，onehot编码是一种计算机处理的二元编码
 y_test_sc=to_categorical(y_test,num_classes=11)
-print('训练集标签大小：',y_train_sc.shape)
-print('测试集标签大小:',y_test_sc.shape)
+print('training label size：',y_train_sc.shape)
+print('testing label size:',y_test_sc.shape)
+
+'''
+add noise to input signals
+'''
+noise_factor = 0.5
+x_train_noisy = x_train + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_train.shape) 
+x_test_noisy = x_test + noise_factor * np.random.normal(loc=0.0, scale=1.0, size=x_test.shape) 
+
 
 ''''
 自编码器模型
 '''
-mymodel=TBAE(x_train,x_test,y_train_sc,y_test_sc,class_n=11,hidden_layer=[50,30],epoc=200,batch_s=100, 
+mymodel=TBAE(x_train_noisy,x_test,y_train_sc,y_test_sc,class_n=11,hidden_layer=[50,30],epoc=200,batch_s=100, 
          lr_a=0.001,decay_a=1e-6)
 
 
